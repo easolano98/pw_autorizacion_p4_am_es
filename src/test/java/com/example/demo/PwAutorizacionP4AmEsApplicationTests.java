95090b7cd9850ce2aad2e449a7b9044db1291f9f
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-09 19:46:54 -0500

    Taller38

---
+++ src/test/java/com/example/demo/PwAutorizacionP4AmEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionP4AmEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
