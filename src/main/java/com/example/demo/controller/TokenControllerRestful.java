d0f7d16a08f0c5d96cff169c68635f3e9035d1cd
easolano98 <solano_erick@hotmail.com>
2023-09-02 10:43:03 -0500

    Examen F2 Autorizacion Solano

--- src/main/java/com/example/demo/controller/TokenControllerRestful.java
+++ src/main/java/com/example/demo/controller/TokenControllerRestful.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.example.demo.service.TO.UsuarioTO;
 
 @RestController
 @RequestMapping("/tokens")
+@CrossOrigin
 public class TokenControllerRestful  {
 	
 	@Autowired
